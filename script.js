2a29bd2612b6c5b787f55d099b485d9adaa34400
ShreyaChannal <shreyachannal2003@gmail.com>
2024-06-11 14:18:06 +0530

    about array

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-console.log("helloo");
-console.log("Heyyzz")
+
+
+//Array -  in js its non-homogeneous
+
+let arr = [1,2,3,4];
+console.log(arr.length);
+console.log(arr[2]);
+
+// .toString(), .join(), .push(); .shift(); .unshift(); .concat(); .sort(); .splice; 
+
+//using loops in array
+
+for(let i = 0; i<arr.length;i++){
+    console.log(arr[i]);
+}
+
+arr.forEach((value,index,arr) => {
+    console.log(value,index,arr)
+}); 
+
+//for in , for of 
+
+//map, filter, reduce
+
