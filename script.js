0fe55ae3249d4e0114f2ce2b6f79a0527419a315
ShreyaChannal <shreyachannal2003@gmail.com>
2024-06-26 18:06:58 +0530

    new

--- script.js
+++ script.js
@@ -2,21 +2,21 @@
 
 //Array -  in js its non-homogeneous
 
-let arr = [1,2,3,4];
-console.log(arr.length);
-console.log(arr[2]);
+// let arr = [1,2,3,4];
+// console.log(arr.length);
+// console.log(arr[2]);
 
 // .toString(), .join(), .push(); .shift(); .unshift(); .concat(); .sort(); .splice; 
 
 //using loops in array
 
-for(let i = 0; i<arr.length;i++){
-    console.log(arr[i]);
-}
+// for(let i = 0; i<arr.length;i++){
+//     console.log(arr[i]);
+// }
 
-arr.forEach((value,index,arr) => {
-    console.log(value,index,arr)
-}); 
+// arr.forEach((value,index,arr) => {
+//     console.log(value,index,arr)
+// }); 
 
 //for in , for of 
 
@@ -25,3 +25,4 @@ arr.forEach((value,index,arr) => {
 //DOM - to target elements using js to change css, etc
 
 
+document.getElementsByClassName("box").style.backgroundColor = "red"
